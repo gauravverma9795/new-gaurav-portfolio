5d949bb9770176fe63213f3f928d4649404c9c16
gauravverma9795 <gauravverma9795@gmail.com>
2024-10-22 11:55:18 +0530

    initial update

---
+++ src/components/About.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import ComputersCanvas from './canvas/Computers';
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+  variants={fadeIn("", "", 0.1, 1)}
+  className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+>
+  I'm a computer science student from IET Lucknow. I'm a passionate Full Stack Web Developer and Competitive Programmer. Over 6-8 months of internship experience, I have dedicated myself to mastering key technologies required for Full Stack Web Development. My skills include React.js, Next.js, Node.js, Express.js, MySQL, MongoDB, PostgreSQL, HTML, CSS, and JavaScript.
+</motion.p>
+
+<motion.div
+  variants={fadeIn("", "", 0.1, 1)}
+  className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+>
+  <p>
+    <span className="text-white">Degree:</span> B-Tech in Computer Science and Engineering (AI) &nbsp; <span className="text-white">CGPA:</span> 8.8
+  </p>
+  <p className='mt-2'>
+    <span className="text-white">Email:</span> manusaini22092003@gmail.com
+  </p>
+</motion.div>
+
+        
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,350 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  tcs,
+  intelligence,
+  sun,
+  payment,
+  intelligence2,
+  rental,
+  
+  
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Experience",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Mern Stack Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Competitive Programmer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Software Development Internship",
+    company_name: "Bluestock Fintech",
+    icon: "bfelogo.jpeg",
+    iconBg: "#fcfafa",
+    date: "Jun 2024- Aug 2024",
+    link:"https://drive.google.com/file/d/1-RDb_GBbw86BneAL1hntM36nPUx1owm-/view",
+    points: [
+      // "Developing and maintaining web applications using React.js, Next.js, Express.js and other related technologies.",
+      // "Developed and maintained reliable web applications with user-friendly interfaces using ReactJS, Next.js, and Tailwind CSS, collaborating with cross-functional teams to meet project requirements",
+      // "Managed databases (MySQL, MongoDB) and developed scalable JavaScript applications using Express.js, ensuring data integrity, security, and maintainability.",
+      // "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      // // "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Designed and developed a stock market website using Python, Django, and Figma, etc.",
+      "Gained hands-on experience in stock market app development, contributing to real-world projects.",
+      "Tech used - Python, Django, Figma",
+    ],
+},
+  {
+    title: "Software Developer Intern",
+    company_name: "Hashedbit Innovation",
+    icon: "hilogo.jpeg",
+    iconBg: "#d4cfcf",
+    date: "Jan 2024- May 2024",
+    link:"https://drive.google.com/file/d/1_D66BYcIcNs8K6ecJy4ReC6aLk55vDfC/view",
+    points: [
+        "Developing and maintaining web applications using React.js, Next.js, Express.js and other related technologies.",
+      "Developed and maintained reliable web applications with user-friendly interfaces using ReactJS, Next.js, and Tailwind CSS, collaborating with cross-functional teams to meet project requirements",
+      "Managed databases (MySQL, MongoDB) and developed scalable JavaScript applications using Express.js, ensuring data integrity, security, and maintainability.",
+      "Tech used - Reactjs, Nodejs, Expressjs, MySQL, CSS, Javascript."
+    ],
+  },
+  {
+    title: "Full Stack Developer Intern",
+    company_name: "Octanet Services Pvt Ltd",
+    icon: "oct.jpeg",
+    iconBg: "#fcfafa",
+    date: "October 2023 - December 2023", 
+    link:"https://drive.google.com/file/d/1PFSn4BcwO8sgodhpvPh1LZk9-p3_Yx5a/view",
+    points: [
+      "Collaborated with a team to create and maintain web applications, gaining hands-on experience in HTML5, CSS3, and JavaScript.",
+      "Assisted in designing and developing user interfaces with React.JS, creating interactive and responsive web experiences.",
+      "Contributed to web application development and maintenance, improving performance through active participation in codebase enhancements and troubleshooting.",
+      "Gained exposure to frameworks like Bootstrap by helping create visually appealing and user-friendly web interfaces.",
+    ],
+  },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "After Jaser optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Web Development Certificate",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "SDE.png",
+    link:"https://www.hackerrank.com/certificates/afc7a78f4549",
+  },
+  {
+    testimonial:
+      "After Jaser optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Web Development Certificate",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "React.png",
+    link:"https://www.hackerrank.com/certificates/e06e6db1c752",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Jaser does.",
+    name: "SDE Intern Certificate",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "SIH.jpg",
+    link:"https://drive.google.com/file/d/1v51Oe5MVg9uZ4pxgyubYlzgteeHabQIj/view",
+  },
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Jaser proved me wrong.",
+    name: "Smart India Hackathon",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "BI.jpg",
+    link:"https://drive.google.com/file/d/1-RDb_GBbw86BneAL1hntM36nPUx1owm-/view",
+  },
+  {
+    testimonial:
+      "After Jaser optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Web Development Certificate",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "Oct.jpg",
+    link:"https://drive.google.com/file/d/1PFSn4BcwO8sgodhpvPh1LZk9-p3_Yx5a/view",
+  },
+  {
+    testimonial:
+      "After Jaser optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Web Development Certificate",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "Python.png",
+    link:"https://drive.google.com/file/d/1v722kvhrcnao2bMJ8xBaRfIMbSZ8kklB/view"
+  },
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Jaser proved me wrong.",
+    name: "Smart India Hackathon",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "GoldSach.jpg",
+    link:"https://drive.google.com/file/d/1vM9fG0nW5ihc8zJGaVPSGULxKVm9hnhE/view",
+  },
+  {
+    testimonial:
+      "After Jaser optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Web Development Certificate",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "Accenture.png",
+    link:"https://drive.google.com/file/d/1XB1ARjHSXhT_l6nlFZqs4cyDiMpAvGGc/view",
+  },
+  {
+    testimonial:
+      "After Jaser optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Web Development Certificate",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "CIT .jpg",
+    link:"https://drive.google.com/file/d/1X8maaMJ8H8Hpxo2PYAvRWW7-pq4RAgL7/view"
+  },
+];
+
+const projects = [
+  {
+    name: "Real Time Chat App",
+    description:
+      // "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "I developed a chat application with real-time, two-way communication, allowing message editing, deletion, and online status display. It also includes a search functionality for efficiently managing and locating contacts Tech Used- ReactJS, NodeJS, MongoDB, ExpressJS, Redux, React-Hooks,Tailwind CSS, APIs",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "express.js",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "chatt.jpg",
+    link:"https://real-time-chat-frontend-1.vercel.app/",
+    source_code_link: "https://github.com/ManuSaini9084/Real-Time-Chat-Frontend",
+  },
+  {
+    name: "Rail Ticket Karo",
+    description:
+      "The online railway ticket reservation system is designed to efficiently manage train bookings and provide real-time information on availability, prices, and schedules. It has admin and user panels. It provides easy booking of train ticket. Schedules of train are handled properly. Tech Used- ReactJs, NodeJs, PostgreSQL, APIs",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "express.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "aws",
+        color: "green-text-gradient",
+      },
+    ],
+    image: "rail.jpg",
+    link:"https://github.com/ManuSaini9084/Railway-irctc",
+    source_code_link: "https://github.com/ManuSaini9084/Railway-irctc",
+  },
+  {
+    name: "Empowering India",
+    description:
+      "An inclusive platform to provide information about government schemes, NGOs, and collaboration opportunities to support marginalized communities. It provides multilingual support, chatbots facilities, authentication as well. Tech Used- ReactJs, NodeJs,ExpressJs, APIs, MongoDB, Tailwind CSS",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "bootsrap5",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "incl.avif",
+    link:"https://inclusion-empowering-india-wfen.onrender.com/",
+    source_code_link: "https://github.com/yashpatel139/Inclusion",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
